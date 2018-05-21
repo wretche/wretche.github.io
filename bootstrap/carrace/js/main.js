63bce2bf575d4782c1e845d5183ed808875260ae
wretche <baxter79@gmail.com>
2018-05-21 23:33:49 +0900

    update

--- bootstrap/carrace/js/main.js
+++ bootstrap/carrace/js/main.js
@@ -19,8 +19,14 @@
     setInterval(updateAll, 1000/framesPerSecond);
 
     setupInput();
-    blueCar.reset(carPic);
-    greenCar.reset(otherCarPic);
+
+    loadLevel(levelOne);
+  }
+
+  function loadLevel(whichLevel) {
+    trackGrid = whichLevel.slice();
+    blueCar.reset(carPic, "Blue Storm");
+    greenCar.reset(otherCarPic, "Green Machine");
   }
 
   function updateAll() {
--- bootstrap/carrace/js/track.js
+++ bootstrap/carrace/js/track.js
@@ -4,7 +4,7 @@ const TRACK_H = 40;
 const TRACK_COLS = 20;
 const TRACK_ROWS = 15;
 
-var trackGrid = [4,4,4,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,4,
+var levelOne = [ 4,4,4,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,4,
                  4,4,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,
                  4,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
                  1,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,0,0,0,1,
@@ -19,6 +19,7 @@ var trackGrid = [4,4,4,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,4,
                  0,3,0,0,0,0,1,4,1,0,0,0,1,1,0,0,0,0,0,1,
                  0,3,0,0,0,0,1,4,4,1,1,1,1,1,1,0,0,0,1,1,
                  1,1,1,1,1,1,1,4,4,4,4,4,4,4,1,1,1,1,1,4];
+var trackGrid = [];
 
 const TRACK_ROAD = 0;
 const TRACK_WALL = 1;
@@ -50,6 +51,16 @@ function drawTracks() {
   }
 }
 
+function returnTileTypeAtColRow(col,row) {
+  if(col >= 0 && col < TRACK_COLS &&
+    row >= 0 && row < TRACK_ROWS) {
+    var trackIndexUnderCoord = rowColToArrayIndex(col, row);
+  return (trackGrid[trackIndexUnderCoord]);
+  }else {
+    return TRACK_WALL;
+  }
+}
+
 function carTrackHandeling(whichCar) {
   var carTrackCol = Math.floor(whichCar.x / TRACK_W);
   var carTrackRow = Math.floor(whichCar.y / TRACK_H);
@@ -57,25 +68,20 @@ function carTrackHandeling(whichCar) {
 
   if (carTrackCol >= 0 && carTrackCol < TRACK_COLS &&
       carTrackRow >= 0 && carTrackRow < TRACK_ROWS) {
+      var tileHere = returnTileTypeAtColRow(carTrackCol,carTrackRow)
 
-    if(isObstacleAtColRow(carTrackCol,carTrackRow)) {
+    if(tileHere == TRACK_GOAL) {
+      console.log(whichCar.name + " WINS!");
+      loadLevel(levelOne);
+    } else if (tileHere != TRACK_ROAD) {
       whichCar.x -= Math.cos(whichCar.ang) * whichCar.speed;
       whichCar.y -= Math.sin(whichCar.ang) * whichCar.speed;
-
       whichCar.speed *= -0.5;
     }
   }
 }
 
-function isObstacleAtColRow(col,row) {
-  if(col >= 0 && col < TRACK_COLS &&
-    row >= 0 && row < TRACK_ROWS) {
-    var trackIndexUnderCoord = rowColToArrayIndex(col, row);
-  return (trackGrid[trackIndexUnderCoord] != TRACK_ROAD);
-  }else {
-    return false;
-  }
-}
+
 function rowColToArrayIndex(col, row) {
   return col + TRACK_COLS * row;
 }
